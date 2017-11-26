1f3a4b80be71d8a02fd17a7602c161e569143563
David Barragán Merino <david.barragan@kaleidos.net>
2017-11-26 13:14:29 +0100

    Create project :baby::baby_bottle:

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template src='./App.html'></template>
+<style src='./App.css'></style>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
---
+++ src/components/PhMap/PhMap.vue
@@ -0,0 +1,21 @@
+<template src='./PhMap.html'></template>
+<style src='./PhMap.css'></style>
+
+<script>
+import Vue2Leaflet from 'vue2-leaflet'
+
+export default {
+  name: 'HfMap',
+  components: {
+    'v-map': Vue2Leaflet.Map,
+    'v-tilelayer': Vue2Leaflet.TileLayer,
+    'v-marker': Vue2Leaflet.Marker
+  },
+  data: () => ({
+    zoom: 13,
+    center: [47.413220, -1.219482],
+    url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  })
+}
+</script>
